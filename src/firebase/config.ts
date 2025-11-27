e7b5f1a5f7991c9fde28332a92f9e08137bdf4cb
Jim Joshua Boquil <2201101389@student.buksu.edu.ph>
2025-11-27 02:41:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3987841126-cd634",
+  "appId": "1:1046720419416:web:62b9663fba7f1c989b1c2d",
+  "apiKey": "AIzaSyAkKn4IMD5CZRwElraF8qdNXxJ2YCFVjJc",
+  "authDomain": "studio-3987841126-cd634.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1046720419416"
+};
