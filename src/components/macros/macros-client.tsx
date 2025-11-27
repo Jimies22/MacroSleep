6dc6ffc0878a6eaa6e51b74d15cc2652b7f74555
Jim Joshua Boquil <2201101389@student.buksu.edu.ph>
2025-11-27 04:48:14 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/components/macros/macros-client.tsx
+++ src/components/macros/macros-client.tsx
@@ -13,6 +13,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
 import { Skeleton } from "../ui/skeleton";
 import { Loader2, PlusCircle, Search } from "lucide-react";
