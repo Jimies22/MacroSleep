ba19467b4b8039dc995c3af6134dc6fe2ee10712
Jimies22 <2201101389@student.buksu.edu.ph>
2025-12-07 12:44:12 +0800

    Profile check issue 1

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1046720419416:web:62b9663fba7f1c989b1c2d",
   "apiKey": "AIzaSyAkKn4IMD5CZRwElraF8qdNXxJ2YCFVjJc",
   "authDomain": "studio-3987841126-cd634.firebaseapp.com",
+  "storageBucket": "studio-3987841126-cd634.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1046720419416"
 };
